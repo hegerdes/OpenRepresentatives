5c6d9408920b1c3e2bd572b1d3457b530398d337
Henrik Gerdes <hegerdes@uni-osnabrueck.de>
2021-04-16 18:42:43 +0000

    Added DB init

--- wsgi.py
+++ wsgi.py
@@ -2,13 +2,13 @@ from app import app, start, checkDB
 import dotenv
 import os
 import threading
-
+from db.src import fillDB19 as db_worker
 
 worker = threading.Thread(target=checkDB, daemon=True)
-print('test')
 if os.environ.get('FLASK_ENV', 'development') == 'production':
     worker.start()
 
+db_worker.updateDB()
 
 def initApp():
     if os.environ.get('FLASK_ENV', 'development') == 'development':
