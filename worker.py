abc803791499f92b36aac04d88e0465305e72766
Henrik Gerdes <hegerdes@uni-osnabrueck.de>
2021-04-16 21:03:53 +0200

    No threads in worker

--- worker.py
+++ worker.py
@@ -1,8 +1,12 @@
 from app import checkDB
 import dotenv
 import os
+import time
 import threading
 from db.src import fillDB19 as db_worker
+from lib.const import DB_UPDATE_FREQUENCY
 
-worker = threading.Thread(target=checkDB, daemon=True)
-worker.start()
+while True:
+    print('Starting DB update check...')
+    db_worker.updateDB()
+    time.sleep(DB_UPDATE_FREQUENCY)
